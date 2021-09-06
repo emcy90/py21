6238806e34612c18ccda464ff11bb67339ee3fe6
Emma Skog <emma@emcy.se>
2021-09-06 17:51:41 +0200

    added exits, directions, room info etc.

--- pythonTextGame/main.py
+++ pythonTextGame/main.py
@@ -1,16 +1,41 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from rooms import the_map
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
+    row = 0
+    col = 0
+    running = True
+
+    while running:
+        print("Welcome to the Zelda text adventure")
+        print("You are now in", the_map[row][col]['description'])
+        print("There are exits to the", the_map[row][col]['exits'])
+
+        command = input("> ")
+        command_parts = command.split()
+
+        if command_parts[0].lower() == "go":
+            if command_parts[1].lower() in the_map[row][col]['exits']:
+                if command_parts[1].lower() == "north":
+                    row -= 1
+                elif command_parts[1].lower() == "south":
+                    row += 1
+                elif command_parts[1].lower() == "east":
+                    col -= 1
+                elif command_parts[1].lower() == "west":
+                    col += 1
+            else:
+                print("You can't go in that direction. try again")
+        elif command_parts[0].lower() == "quit":
+            running = False
+        else:
+            print("I don't understand", command_parts[0])
+
+    print("Thank you for playing this game. Now go do something else like cuddle with a kitty ;) ")
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Emcy')
+    main()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ pythonTextGame/rooms.py
@@ -0,0 +1,25 @@
+room1 = {
+    "description": "bedroom",
+    "exits": ["north", "west"]
+}
+
+room2 = {
+    "description": "bathroom",
+    "exits": ["north", "east",  "south"]
+}
+
+room3 = {
+    "description": "kitchen",
+    "exits": ["west", "south"]
+}
+
+room4 = {
+    "description": "living room",
+    "exits": ["east", "south"]
+}
+
+
+the_map = [
+    [room1, room2],
+    [room3, room4]
+]
