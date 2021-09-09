807f58aa76d8c63e985e9d14b0d2406fc002cfb6
Emma Skog <emma@emcy.se>
2021-09-09 13:34:59 +0200

    updated but not entirely working. will fix after class

--- pythonTextGame/main.py
+++ pythonTextGame/main.py
@@ -1,32 +1,63 @@
 from rooms import the_map
 
 
+def go(row, col, direction):
+    if direction in the_map[row][col]['exits']:
+        if direction == "north":
+            row -= 1
+        elif direction == "south":
+            row += 1
+        elif direction == "east":
+            col -= 1
+        elif direction == "west":
+            col += 1
+    else:
+        print("You can't go in that direction. try again")
+    return row, col
+
+
+def get(row, col, item, inventory):
+    if item in the_map[row][col]['items']:
+        print("you picked up the", inventory)
+        the_map[row][col]['items'].remove(item)
+        inventory.append(item)
+    else:
+        print("There is no", inventory, "in this room")
+
+
 def main():
     row = 0
     col = 0
     running = True
     print("Welcome to the Zelda text adventure")
+    inventory = []
 
     while running:
         print("You are now in", the_map[row][col]['description'])
+        print("Items available here are ", the_map[row][col]['items'])
         print("There are exits to the", the_map[row][col]['exits'])
 
         command = input("> ")
         command_parts = command.split()
 
-        if command_parts[0].lower() == "go":
-            if command_parts[1].lower() in the_map[row][col]['exits']:
-                if command_parts[1].lower() == "north":
-                    row -= 1
-                elif command_parts[1].lower() == "south":
-                    row += 1
-                elif command_parts[1].lower() == "east":
-                    col -= 1
-                elif command_parts[1].lower() == "west":
-                    col += 1
+        main_command = command_parts[0].lower()
+
+        if main_command == "go":
+            row, col = go(row, col, command_parts[1].lower())
+        elif main_command == "get":
+            get(row, col, command_parts[1].lower(), inventory)
+        elif main_command == "drop":
+            sub_command = command_parts[1].lower()
+            if sub_command in inventory:
+                print("you dropped the", sub_command, "to the floor")
+                inventory.remove(sub_command)
+                the_map[row][col]['items'].append(sub_command)
             else:
-                print("You can't go in that direction. try again")
-        elif command_parts[0].lower() == "quit":
+                print("there is no", sub_command, "in your inventory")
+        elif main_command == "inventory":
+            for item in inventory:
+                print(item)
+        elif main_command == "quit":
             running = False
         else:
             print("I don't understand", command_parts[0])
--- pythonTextGame/rooms.py
+++ pythonTextGame/rooms.py
@@ -1,20 +1,24 @@
 room1 = {
     "description": "bedroom",
+    "items": ["alarm"],
     "exits": ["north", "west"]
 }
 
 room2 = {
     "description": "bathroom",
+    "items": ["bathrobe"],
     "exits": ["north", "east",  "south"]
 }
 
 room3 = {
     "description": "kitchen",
+    "items": ["food"],
     "exits": ["west", "south"]
 }
 
 room4 = {
     "description": "living room",
+    "items": ["laptop"],
     "exits": ["east", "south"]
 }
 
