914eab5a987f9f8f5a03664fa84ff0c05ec5ee44
Emma Skog <emma@emcy.se>
2021-09-10 15:13:02 +0200

    updated for lecture 4

--- connectfour/main.py
+++ connectfour/main.py
@@ -11,24 +11,24 @@ def main():
         [0, 0, 0, 0, 0, 0, 0]
     ]
 
+    player_in_turn = 1
     running = True
     while running:
+        for row in board:
+            for cell in row:
+                color = "white"
+                if cell == 1:
+                    color = "magenta"
+                elif cell == 2:
+                    color = "cyan"
+                color_print(color, "0", end=" ")
+            print()
+        color_print("green", "A B C D E F G")
+
         print("please select a column(A-F) ")
-        choice = input("> ")
-        main_choice = choice.lower():
-        if main_choice == "a":
+        move = input(f"Player {player_in_turn}, pick a column: ")
 
-
-    for row in board:
-        for cell in row:
-            color = "white"
-            if cell == 1:
-                color = "magenta"
-            elif cell == 2:
-                color = "cyan"
-            color_print(color, "0", end=" ")
-        print()
-    color_print("green", "A B C D E F G")
+        player_in_turn = 2 if player_in_turn == 1 else 1
 
 
 if __name__ == '__main__':
